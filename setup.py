e2d6b9bdab6154ec17670a7fda8a73119505c80f
Sivan Becker <sivanbecker@gmail.com>
2017-09-26 13:30:14 +0300

    fix setup

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup
 
 setup(
     name='penv',
-    version = '0.0.6',
+    version = '0.0.7',
     author = 'Sivan Becker',
     author_email = 'sivanbecker@gmail.com',
     description = ("dhcp/ldap stuff, dhcpawn populate"),
     license = "BSD",
     keywords = "dhcp ldap",
-    py_modules=['penv'],
+    packages=['penv'],
     install_requires=[
         'Click',
         'pyldap',
